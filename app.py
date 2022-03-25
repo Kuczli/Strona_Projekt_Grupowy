6e74c4d531db7a4311923fdcd00a2dc4004ea3a6
owczarek72144 <75564904+owczarek72144@users.noreply.github.com>
2022-03-25 11:49:13 +0100

    Dodanie obsługi maila

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail,Message
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////customers.db'
 db = SQLAlchemy(app)
@@ -20,6 +21,9 @@ def home():
     if request.method == 'POST':
         name = request.form.get('inlineFormInputName2')
         email = request.form.get('inlineFormInputGroupUsername2')
+        checkopt1 = False
+        checkopt2 = False
+        checkopt3 = False
         if request.form.get('checkopt1') != None:
             checkopt1 =True
         if request.form.get('checkopt1') != None:
@@ -31,9 +35,31 @@ def home():
         cu = Custormer(name = name,email = email,checkopt1 = checkopt1,checkopt2=checkopt2,checkopt3=checkopt3,message=message)
         db.session.add(cu)
         db.session.commit()
+        subject = 'Kurs fotografii'
+        name = name
+        message = message
+        recipients = ['vassilli.zaitsev@gmail.com']
+        mesage_to_admin = Message(subject=subject,
+                                  sender=app.config.get("MAIL_USERNAME"),
+                                  recipients=recipients,  # replace with your email for testing
+                                  body="Message from: " + name + "\n" + "email: " + email + "\n" + "Message:" + message)
+        message_to_sender = Message(subject="Dziękuje za wysłanie wiadomośći",
+                                    sender=app.config.get("MAIL_USERNAME"),
+                                    recipients=list(email.split(" ")), # replace with your email for testing
+                                    body="Dziękuję za wiadomość, wkrótce odpiszę")
+        mail.send(mesage_to_admin)
+        mail.send(message_to_sender)
 
     return render_template("home.html")
 
-
+# email configuration
+app.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT=465,
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME='vassilli.zaitsev@gmail.com',
+    MAIL_PASSWORD='qplzxmdxmzakrwei'
+)
+mail = Mail(app)
 if __name__ == '__main__':
     app.run()
